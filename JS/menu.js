b1ae94d1f062879c05f27d9ffe5d309bbb666fbf
Omphile Ofentse Magabeane <91537564+Fefeswork@users.noreply.github.com>
2024-04-28 01:17:51 +0200

    testing again
    Let's hope this works!!

---
+++ JS/menu.js
@@ -0,0 +1,27 @@
+const root = "/WSOA3028A_2306258"
+
+const menuItems = [
+    { name: "Home", href: root + "/index.html" },
+    //Type Coercion - it is taking a const, adding it to a string and making it the string it expects
+    { name: "Blog", href: `${root}/Blogs/blog.html` },
+    //back-tick syntax (template strings) `...` - allows the use of whitespace, ", ', interpolation, and substitution
+    //allows for string interpolation ${ } - automatically replaces variables and expressions with real values
+    { name: "Essays", href: `${root}/essays/ind.html` },
+    { name: "Portfolio", href: `${root}/portfolio/index.html` },
+    { name: "Design", href: `${root}/design/index.html` },
+]
+export function initialise(currentPage) {
+    const nav = document.querySelector("header > nav")
+    const ul = document.createElement("ul")
+    for (let menuItem of menuItems) {
+        const li = document.createElement("li")
+        if (currentPage != menuItem.name) {
+            const a = document.createElement("a")
+            a.innerText = menuItem.name
+            a.setAttribute("href", menuItem.href)
+            li.appendChild(a)
+        } else { li.innerText = menuItem.name }
+        ul.appendChild(li)
+    }
+    nav.appendChild(ul)
+}
---
+++ JS/onclick.js
@@ -0,0 +1,21 @@
+
+// this code works to change my text on button click (just a note :) )
+
+function changeText() {
+    x = document.getElementById("myDIV");
+   if (x.innerHTML === "Hello") {
+     x.innerHTML = "Swapped text!";
+   } else if (x.innerHTML === "Swapped Text!") {
+     x.innerHTML = "Girl";
+    } else if (x.innerHTML === "Girl") {
+        x.innerHTML = "Me";
+    } else if (x.innerHTML === "Me") {
+        x.innerHTML = "Him";
+    } else if (x.innerHTML === "Him") {
+        x.innerHTML = "Us";
+    } else if (x.innerHTML === "Us") {
+        x.innerHTML = "blah";
+    } else {
+     x.innerHTML= "Hiii"
+   }
+ }
