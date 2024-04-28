f6ae90fa1396f94bb058694d017fdc395b28c651
Omphile Ofentse Magabeane <2306258@students.wits.ac.za>
2024-04-28 02:41:08 +0200

    profile and onclick function

--- JS/onclick.js
+++ JS/onclick.js
@@ -3,18 +3,24 @@
 
 function changeText() {
     x = document.getElementById("myDIV");
-   if (x.innerHTML === "Hello") {
+   if (x.innerHTML === "I like Puppies") {
      x.innerHTML = "Swapped text!";
+
    } else if (x.innerHTML === "Swapped Text!") {
      x.innerHTML = "Girl";
+
     } else if (x.innerHTML === "Girl") {
         x.innerHTML = "Me";
+
     } else if (x.innerHTML === "Me") {
         x.innerHTML = "Him";
+
     } else if (x.innerHTML === "Him") {
         x.innerHTML = "Us";
+
     } else if (x.innerHTML === "Us") {
         x.innerHTML = "blah";
+
     } else {
      x.innerHTML= "Hiii"
    }
