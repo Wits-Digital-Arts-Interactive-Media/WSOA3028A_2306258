597b8fbf3393742a24198573a7acb315cf315b2a
Omphile Ofentse Magabeane <2306258@students.wits.ac.za>
2024-04-29 05:20:14 +0200

    because comments are very helpful

--- JS/blognav.js
+++ JS/blognav.js
@@ -39,3 +39,36 @@ export function blogMenu()
 
     menu.appendChild(ul) 
 }
+
+const navData = [
+    { title: "Home", link: "https://fefeswork.github.io/IM4A-Internet-Art-A3/home.html"},
+    { title: "Theory", link: "https://fefeswork.github.io/IM4A-Internet-Art-A3/Theory/theory.html"},
+    { title: "Internet Art", link: "https://fefeswork.github.io/IM4A-Internet-Art-A3/Internet Art/IAHome.html"},
+    { title: "Design", link: "https://fefeswork.github.io/IM4A-Internet-Art-A3/Design/design.html"},
+]
+
+
+// I was going to try and use this method below but i struggled to get it to work...
+// it's recycled code i had from anohter priject i worked on
+
+
+// const navElement = document.getElementById("nav-Id");
+
+// let list = document.createElement("ul");
+
+// for(var i = 0; i < navData.length; i++){
+
+//     let listItemLink = document.createElement("a");
+//     listItemLink.innerText = navData[i].title;
+//     listItemLink.setAttribute("href", navData[i].link);
+    
+//     let listItem = document.createElement("li");
+//     listItem.appendChild(listItemLink);
+//     list.appendChild(listItem);
+// }
+
+// navElement.appendChild(list);
+
+// //Dev notes
+
+// // Navigation data, you are essentially using relative referenicng for the links//
