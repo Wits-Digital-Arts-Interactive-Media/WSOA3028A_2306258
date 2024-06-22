250b473c29cebdd1bd73c5ce2e6f8c08f78bc806
Omphile Ofentse Magabeane <2306258@students.wits.ac.za>
2024-06-22 14:18:06 +0200

    update blog navigation

--- JS/blogsnav.js
+++ JS/blogsnav.js
@@ -1,14 +1,5 @@
-// Wait for the DOM to fully load
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Get the button element by its ID
-    const button = document.getElementById('mybutton');
+//function that controls navigation from blog posts page.
 
-    // Define the function to be executed when the button is clicked
-    function buttonClickHandler() {
-        alert('Button was clicked!');
-        // Add more functionality here as needed
-    }
-
-    // Add the onclick event listener to the button
-    button.onclick = buttonClickHandler;
-});
+function navigateToPage(url) {
+    window.location.href = url;
+}
