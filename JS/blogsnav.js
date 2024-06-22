d9f8a85493618b71454acdd0b0e643d2ade52f6c
Omphile Ofentse Magabeane <2306258@students.wits.ac.za>
2024-06-22 14:19:54 +0200

    update navigation

--- JS/blogsnav.js
+++ JS/blogsnav.js
@@ -1,4 +1,5 @@
 //function that controls navigation from blog posts page.
+// also controls next and previous button for navigation
 
 function navigateToPage(url) {
     window.location.href = url;
